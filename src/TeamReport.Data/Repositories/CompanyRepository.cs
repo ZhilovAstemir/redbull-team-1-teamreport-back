b27ee489b0eea8f05dd90a0c6a79e73021017dc0
milanamaksina <m_maksinaa@mail.ru>
2022-10-03 01:32:34 +0300

    feat: update company repo

--- src/TeamReport.Data/Repositories/CompanyRepository.cs
+++ src/TeamReport.Data/Repositories/CompanyRepository.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using Microsoft.EntityFrameworkCore;
+using redbull_team_1_teamreport_back.Domain.Entities;
+using redbull_team_1_teamreport_back.Domain.Persistence;
+using redbull_team_1_teamreport_back.Domain.Repositories.Interfaces;
+
+namespace redbull_team_1_teamreport_back.Domain.Repositories;
+public class CompanyRepository
+{
+
+}
