893ca892d3174605f0360f49adaa5ab1d994defa
alevshinskii <66357905+alevshinskii@users.noreply.github.com>
2022-10-07 09:35:24 +0300

    feat: tests added

--- tests/TeamReport.Domain.Tests/Services/TeamServiceTest.cs
+++ tests/TeamReport.Domain.Tests/Services/TeamServiceTest.cs
@@ -1,6 +1,9 @@
-﻿using FluentAssertions;
+﻿using AutoMapper;
+using FluentAssertions;
+using TeamReport.Data.Entities;
 using TeamReport.Data.Persistence;
 using TeamReport.Data.Repositories;
+using TeamReport.Data.Repositories.Interfaces;
 using TeamReport.Domain.Services;
 
 namespace TeamReport.Domain.Tests.Services;
@@ -9,17 +12,25 @@ public class TeamServiceTest
 {
     private readonly ServiceTestFixture _fixture;
     private readonly ApplicationDbContext _context;
+    private readonly IMemberRepository _memberRepository;
+    private readonly ILeadershipRepository _leadershipRepository;
+    private readonly ICompanyRepository _companyRepository;
+    private readonly IMapper _mapper;
 
     public TeamServiceTest()
     {
         _fixture = new ServiceTestFixture();
         _context = _fixture.GetContext();
+        _memberRepository = new MemberRepository(_context);
+        _leadershipRepository = new LeadershipRepository(_context);
+        _companyRepository = new CompanyRepository(_context);
+        _mapper = _fixture.GetMapper();
     }
 
     [Fact]
     public void ShouldBeAbleToCreateTeamService()
     {
-        var service = new TeamService(new MemberRepository(_context), new LeadershipRepository(_context), new CompanyRepository(_context), _fixture.GetMapper());
+        var service = new TeamService(_memberRepository, _leadershipRepository, _companyRepository, _mapper);
         service.Should().NotBeNull().And.BeOfType<TeamService>();
     }
 
@@ -28,18 +39,63 @@ public class TeamServiceTest
     {
         _fixture.ClearDatabase();
 
-        var memberRepository = new MemberRepository(_context);
-        var service = new TeamService(memberRepository, new LeadershipRepository(_context), new CompanyRepository(_context), _fixture.GetMapper());
+        var service = new TeamService(_memberRepository, _leadershipRepository, _companyRepository, _mapper);
         service.Should().NotBeNull().And.BeOfType<TeamService>();
 
-        var member = await memberRepository.Create(_fixture.GetMember());
+        var member = await _memberRepository.Create(_fixture.GetMember());
         var member2 = _fixture.GetMember();
         member2.Company = member.Company;
-        await memberRepository.Create(member2);
+        await _memberRepository.Create(member2);
 
         var teamMembers = await service.GetAllTeamMembers(member.Company.Id);
+        teamMembers.Should().HaveCount(2);
     }
 
+    [Fact]
+    public async Task ShouldBeAbleToGetMemberLeaders()
+    {
+        _fixture.ClearDatabase();
+
+        var service = new TeamService(_memberRepository, _leadershipRepository, _companyRepository, _mapper);
+        service.Should().NotBeNull().And.BeOfType<TeamService>();
+
+        var member = await _memberRepository.Create(_fixture.GetMember());
+        var member2 = _fixture.GetMember();
+        member2.Company = member.Company;
+        await _memberRepository.Create(member2);
+        var member3 = _fixture.GetMember();
+        member3.Company = member.Company;
+        await _memberRepository.Create(member3);
+
+        await _leadershipRepository.Create(new Leadership() { Leader = member2, Member = member });
+        await _leadershipRepository.Create(new Leadership() { Leader = member3, Member = member });
 
 
+        var leaders = await service.GetMemberLeaders(member.Id);
+        leaders.Should().HaveCount(2).And.Contain(x => x.Id == member2.Id).And.Contain(x => x.Id == member3.Id);
+    }
+
+    [Fact]
+    public async Task ShouldBeAbleToGetMemberReporters()
+    {
+        _fixture.ClearDatabase();
+
+        var service = new TeamService(_memberRepository, _leadershipRepository, _companyRepository, _mapper);
+        service.Should().NotBeNull().And.BeOfType<TeamService>();
+
+        var member = await _memberRepository.Create(_fixture.GetMember());
+        var member2 = _fixture.GetMember();
+        member2.Company = member.Company;
+        await _memberRepository.Create(member2);
+        var member3 = _fixture.GetMember();
+        member3.Company = member.Company;
+        await _memberRepository.Create(member3);
+
+        await _leadershipRepository.Create(new Leadership() { Leader = member, Member = member2 });
+        await _leadershipRepository.Create(new Leadership() { Leader = member, Member = member3 });
+
+
+        var reporters = await service.GetMemberReporters(member.Id);
+        reporters.Should().HaveCount(2).And.Contain(x => x.Id == member2.Id).And.Contain(x => x.Id == member3.Id);
+    }
 }
